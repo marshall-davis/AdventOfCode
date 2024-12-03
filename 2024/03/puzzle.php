ab7c39bf4786549be5d281b7f003c3d04b430ef5
Marshall Davis <toothlessrebel@gmail.com>
2024-12-03 00:32:07 -0500

    Day 3 Part 2

--- 2024/03/puzzle.php
+++ 2024/03/puzzle.php
@@ -2,14 +2,33 @@
 
 $input = fopen('input.txt', 'r+');
 $memory = file_get_contents('input.txt');
-$matches = [];
 
-preg_match_all('/mul\((\d{1,3}),(\d{1,3})\)/', $memory, $matches);
 
 $sum = 0;
 
+$parts = explode('don\'t()', $memory);
+
+$initial = array_shift($parts);
+preg_match_all('/mul\((\d{1,3}),(\d{1,3})\)/', $initial, $matches);
 foreach ($matches[1] as $occurrence => $match) {
     $sum += $match * $matches[2][$occurrence];
 }
+echo "Initially: $sum\n";
+foreach ($parts as $part => $segment) {
 
-echo $sum. PHP_EOL;
+    $matches = [];
+    $start = strpos($segment, 'do()');
+    if ($start === false) {
+        continue;
+    }
+    $instruction = substr($segment, $start + 4);
+    echo $instruction."\n\n";
+    preg_match_all('/mul\((\d{1,3}),(\d{1,3})\)/', $instruction, $matches);
+    foreach ($matches[1] as $occurrence => $match) {
+        $sum += $match * $matches[2][$occurrence];
+    }
+}
+
+assert(173529487 >= $sum);
+
+echo $sum.PHP_EOL;
