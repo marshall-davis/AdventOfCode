c2af297be5cc23e4d581a8cad007b00f8a651e1e
Marshall Davis <marshall@exposuresoftware.comm>
2024-12-13 23:12:11 -0500

    Day 11 Part 2

--- 2024/11/puzzle.php
+++ 2024/11/puzzle.php
@@ -1,28 +1,46 @@
 <?php
 
 $inputStream = fopen('input.txt', 'r+');
-$stones = explode(' ', trim(fgets($inputStream)));
-echo implode(' ', $stones).PHP_EOL;
-$blinks = 25;
+$stones = array_count_values(explode(' ', trim(fgets($inputStream))));
+$blinks = 75;
 for ($blink = 1; $blink <= $blinks; $blink++) {
-    $transformed = [];
-    $splits = 0;
-    foreach ($stones as $position => $engraving) {
-        if ($engraving === '0') {
-            $transformed[$position + $splits] = 1;
-        } elseif (($length = strlen($engraving)) % 2 === 0) {
-            $first = substr($engraving, 0, $length / 2);
-            $second = substr($engraving, ($length / 2));
-            array_splice($transformed, $position + $splits, 1, [(string) (int) $first, (string) (int) $second]);
-            ++$splits;
-        } else {
-            $transformed[$position + $splits] = $engraving * 2024;
+    echo "Blink $blink\n";
+    $iterator = new ArrayIterator($stones);
+    $additions = [];
+    while ($iterator->valid()) {
+        $value = $iterator->key();
+        if (!is_int($value)) {
+            throw new RuntimeException();
         }
+        if ($value == 0) {
+            /**
+             * Set the number of with a 1 engraving in $stones to be the number
+             * of 0 engravings in $next plus any existing 1s.
+             */
+            $additions[1] = ($additions[1] ?? 0) + $stones[0];
+
+        } elseif (($length = strlen($value)) % 2 === 0) {
+            /**
+             * Split the number into its two halves.
+             */
+            $first = (int) substr($value, 0, $length / 2);
+            $second = (int) substr($value, ($length / 2));
+            /**
+             * Each one of those should be stored, plus any current with that value.
+             */
+            $additions[$first] = ($additions[$first] ?? 0) + $stones[$value];
+            $additions[$second] = ($additions[$second] ?? 0) + $stones[$value];
+        } else {
+            $product = bcmul($value, 2024);
+            $additions[$product] = ($additions[$product] ?? 0) + $stones[$value];
+        }
+        unset($stones[$value]);
+        $iterator->next();
     }
-    $stones = $transformed;
-    echo "Blink $blink: ".count($stones).PHP_EOL;
-//    echo implode(' ', $stones).PHP_EOL.PHP_EOL;
+    foreach ($additions as $value => $count) {
+        $stones[$value] = ($stones[$value] ?? 0) + $count;
+    }
 }
 
-echo "FINAL: ".count($stones).PHP_EOL;
+echo "FINAL: ".array_sum($stones).PHP_EOL;
 
