450daef263103da4bdfccde1ad92fe00cf7af0a3
Marshall Davis <mdavis@teamworks.com>
2025-12-02 01:51:47 -0500

    2025 Day 02 Part 02

--- 2025/02/puzzle.php
+++ 2025/02/puzzle.php
@@ -3,35 +3,47 @@
 declare(strict_types=1);
 
 $pairs = explode(',',file_get_contents('input.txt'));
+//$pairs = explode(',','11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124');
 $pairs = array_map('trim',$pairs);
 
 $invalidIds = [];
+$processed = 0;
 
 foreach($pairs as $pair) {
     [$start,$end] = explode('-',$pair);
+    $valid = [];
 
     foreach(range(intval($start),intval($end)) as $id) {
-        if (strlen((string)$id) % 2 !== 0) {
-            continue;
+        $processed++;
+        if (strlen((string)$id) === 1) {
+            continue; // Cannot repeat a single digit.
         }
         if (count(array_unique(str_split((string)$id))) === 1) {
             $invalidIds[] = $id;
             continue;
         }
 
-        if (strlen((string)$id) === 2) {
-            continue;
-        }
-
-        foreach(str_split((string)$id) as $position => $char) {
-            $splits = array_unique(str_split((string)$id, $position + 1));
-            if (count($splits) === 1 && strlen((string)$id) / strlen($splits[0]) == 2) {
+        for($length = 2;$length < strlen((string)$id);$length++) {
+            $chunks = array_map('implode',array_chunk(str_split((string)$id), $length));
+            $chunks = array_unique($chunks);
+            if (count($chunks) === 1) {
                 $invalidIds[] = $id;
+                break;
             }
         }
+        $valid[] = $id;
     }
+
+    if (count($valid) === 0) {
+        echo "No valid IDs for $pair.\n";
+    }
+//    echo implode("\n",$valid).PHP_EOL;
+//    readline('Continue?');
 }
+
 $invalidIds = array_unique($invalidIds);
-echo implode("\n",$invalidIds).PHP_EOL;
-echo 'Found '.count($invalidIds).' invalid IDs.'.PHP_EOL;
+//echo implode("\n",$invalidIds).PHP_EOL;
+echo 'Found '.count($invalidIds)." invalid IDs of $processed.".PHP_EOL;
 echo 'Sum of invalid IDs: '.array_sum($invalidIds).PHP_EOL;
+assert(array_sum($invalidIds) !== 45283684600);
+assert(array_sum($invalidIds) > 20984710784);
