d93a28076f39c07bb63739382dd658e82c7355cc
Marshall Davis <mdavis@teamworks.com>
2025-12-04 08:21:42 -0500

    2025 Day 04 Part 02

--- 2025/04/puzzle.php
+++ 2025/04/puzzle.php
@@ -11,37 +11,47 @@ while (!feof($input)) {
     $map[] = str_split(trim(fgets($input)));
 }
 
-$accessible = 0;
-foreach ($map as $row => $line) {
-    foreach ($line as $column => $cell) {
-        if ($cell === '.') {
-            continue;
-        }
-
-        $adjacent = 0;
-        echo "Working on $row,$column as $cell.\n";
-        foreach ([-1, 1] as $offset) {
-            $current = $line[$column + $offset] ?? null;
-//            echo "\tOffset $offset $current\n";
-            $adjacent += $current === '@' ? 1 : 0; // Space to the right and right.
-
-            $current = $map[$row + $offset][$column] ?? null;
-//            echo "\tOffset $offset $current\n";
-            $adjacent += $current === '@' ? 1 : 0; // Space below and below.
-
-            $current = $map[$row - 1][$column + $offset] ?? null;
-//            echo "\tOffset $offset $current\n";
-            $adjacent += $current === '@' ? 1 : 0; // Space to Diagonal.
-
-            $current = $map[$row + 1][$column + $offset] ?? null;
-//            echo "\tOffset $offset $current\n";
-            $adjacent += $current === '@' ? 1 : 0; // Space to the lower left.
-        }
-
-        if ($adjacent < 4) {
-            $accessible++;
-        }
-    }
+function removeRolls(array $map): array
+{
+    return array_map(fn(array $line) => array_map(fn(string $cell) => $cell === 'X' ? '.' : $cell, $line), $map);
 }
 
-echo "$accessible accessible spaces.".PHP_EOL;
+$removed = 0;
+do {
+    $accessible = 0;
+    foreach ($map as $row => $line) {
+        foreach ($line as $column => $cell) {
+            if ($cell === '.') {
+                continue;
+            }
+
+            $adjacent = 0;
+//            echo "Working on $row,$column as $cell.\n";
+            foreach ([-1, 1] as $offset) {
+                $current = $line[$column + $offset] ?? null;
+//            echo "\tOffset $offset $current\n";
+                $adjacent += $current !== '@' ? 0 : 1; // Space to the right and right.
+
+                $current = $map[$row + $offset][$column] ?? null;
+//            echo "\tOffset $offset $current\n";
+                $adjacent += $current !== '@' ? 0 : 1; // Space below and below.
+
+                $current = $map[$row - 1][$column + $offset] ?? null;
+//            echo "\tOffset $offset $current\n";
+                $adjacent += $current !== '@' ? 0 : 1; // Space to Diagonal.
+
+                $current = $map[$row + 1][$column + $offset] ?? null;
+//            echo "\tOffset $offset $current\n";
+                $adjacent += $current !== '@' ? 0 : 1; // Space to the lower left.
+            }
+
+            if ($adjacent < 4) {
+                $map[$row][$column] = 'X';
+                $accessible++;
+            }
+        }
+    }
+    $map = removeRolls($map);
+    $removed += $accessible;
+} while ($accessible !== 0);
+echo "$removed rolls removed.".PHP_EOL;
