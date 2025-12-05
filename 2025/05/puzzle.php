bbf9a3bbc70f33e267f4bfdccfe9c7c2aac22ea2
Marshall Davis <mdavis@teamworks.com>
2025-12-05 17:20:25 -0500

    2025 Day 05 Part 02

--- 2025/05/puzzle.php
+++ 2025/05/puzzle.php
@@ -9,17 +9,110 @@ $fresh = [];
 $ingredients = [];
 while ($range = trim(fgets($input))) {
     preg_match('/(\d+)-(\d+)/', $range, $parts);
-    echo "$parts[1] - $parts[2]\n";
     $fresh[] = [$parts[1], $parts[2]];
 }
-while (!feof($input) && $id = intval(trim(fgets($input)))) {
-    foreach($fresh as $range) {
-        if ($id >= $range[0] && $id <= $range[1]) {
-            $ingredients[] = $id;
-            echo "$id is fresh.\n";
-            break;
-        }
-    }
+
+function contains(array $haystack, array $needle): bool
+{
+    return $needle[0] >= $haystack[0] && $needle[1] <= $haystack[1];
 }
 
-echo "There are ".count($ingredients)." ingredients.\n";
+assert(contains([131859446065085, 139731761639513], [134586353952964, 139731761639513]));
+
+function overlaps(int|string $target, array $range): bool
+{
+    return $target >= $range[0] && $target <= $range[1];
+}
+
+assert(overlaps(23388191326133, [20529181418212, 29825483179707]));
+
+function collapseRanges(array $ranges): ?array
+{
+    echo "Collapsing ".count($ranges)." ranges...\n";
+    $collapsed = [$ranges[0]];
+    foreach ($ranges as $i => $range) {
+        foreach ($collapsed as $j => $collapsedRange) {
+            // This is an existing range.
+            if ($collapsedRange[0] === $range[0] && $collapsedRange[1] === $range[1]) {
+                continue 2;
+            }
+            // A previous range is contained within this range.
+            if (contains($range, $collapsedRange)) {
+                $collapsed[$j] = $range;
+                continue;
+            }
+            // The range is contained within a previous range.
+            if (contains($collapsedRange, $range)) {
+                continue 2;
+            }
+            // The range overlaps with the high end of a previous range.
+            if (overlaps($collapsedRange[1], $range)) {
+                $collapsed[$j] = [$collapsedRange[0], $range[1]];
+                continue;
+            }
+            // The range overlaps with the low end of a previous range.
+            if (overlaps($collapsedRange[0], $range)) {
+                $collapsed[$j] = [$range[0], $collapsedRange[1]];
+            }
+        }
+
+        // It's a new range.
+        $collapsed[] = $range;
+    }
+
+    return count($collapsed) === count($ranges) ? null : $collapsed;
+}
+
+function joinRanges(array $ranges): array
+{
+    $result = [];
+    foreach ($ranges as $range) {
+        $end = array_last($result)[1] ?? null;
+        if ($end && (($end + 1) === intval($range[0]))) {
+            $result[array_key_last($result)] = [array_last($result)[0], $range[1]];
+        } else {
+            $result[] = $range;
+        }
+    }
+
+    return $result;
+}
+
+echo "Starting with ".count($fresh)." ranges.\n";
+while ($collapsed = collapseRanges($fresh)) {
+    $fresh = $collapsed;
+    uasort($fresh, fn($a, $b) => intval($a[1]) - intval($b[1]));
+    $fresh = joinRanges(array_values($fresh));
+}
+echo "Collapsed to ".count($fresh)." ranges.\n";
+
+foreach ($fresh as $line => $range) {
+    assert(intval($range[0]) <= intval($range[1]));
+}
+
+//foreach ($fresh as $row => $range) {
+//    echo "Range $row:\t{$range[0]}\t-\t{$range[1]}\n";
+//}
+
+$total = 0;
+foreach ($fresh as $range) {
+    $total += intval($range[1]) - intval($range[0]) + 1;
+}
+
+$fresh = array_values($fresh);
+
+try {
+    assert(count(array_unique(array_column($fresh, 0))) === count($fresh), 'Duplicated range starts.');
+    assert(count(array_unique(array_column($fresh, 1))) === count($fresh), 'Duplicated range ends.');
+    foreach ($fresh as $row => $range) {
+        assert(intval($range[1]) < (intval($fresh[$row + 1][0] ?? PHP_INT_MAX)), "Row $row overlaps the next.\n");
+    }
+    assert($total < 437298360170255, "$total is too high!");
+    assert($total !== 351022724181788, "$total is wrong!");
+    assert($total !== 343860644368880, "$total is wrong!");
+} catch (\AssertionError $e) {
+    echo $e->getMessage().PHP_EOL;
+    exit;
+}
+
+echo "There are $total fresh ingredients.\n";
